8f395894767b62f8dcf579e7faad6620a8e235aa
Pavan678-github <61405632+Pavan678-github@users.noreply.github.com>
2022-06-22 11:17:05 +0530

    up6

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,8 +1,12 @@
 # Get the private key from the environment variable
-echo "Setting up Production Connection..."
-mkdir keys
-echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
+#echo "Setting up Production Connection..."
+#mkdir keys
+#echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
-echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+#echo "Authenticating..."
+#sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+echo "################Authenticating with Org"
+echo $url > login.txt
+sfdx auth:sfdxurl:store -a deployment -f login.txt
+rm -rf login.txt
